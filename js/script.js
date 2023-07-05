e98254f687ac7137ed56b00005d27a89c31ff6bd
Sofia de Aurteneche <114301999+deaaurteneche@users.noreply.github.com>
2023-07-05 01:01:32 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
-let canvas = document.getElementById("canvas")
+//let canvas = document.getElementById("canvas")
 
-canvas.style.backgroundColor = "red"
+//canvas.style.backgroundColor = "red"
+
+
+let ticketNumbers = Array.from(document.getElementsByClassName ("ticket-number"))
+    ticketNumbers.forEach(element => {
+        element.textContent = Math.floor(Math.random()*4)
+    });
+
+
+/*Todos los comentarios mulilineas tienen que estar cerrados o terminados pq sino da error y no funciona*/
